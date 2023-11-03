d1b8c3aac92fead0def8b9cebc54c48a7b25e505
Haku <89026040+sakanaJA@users.noreply.github.com>
2023-11-03 20:39:21 +0900

    勉強だお

---
+++ src/test.java
@@ -0,0 +1,30 @@
+
+public class test {
+	public static void main(String args[]) {
+		//3-1
+		//		for (int num = 1; num < 10; num++) {
+		//			for (int i = 1; i < 10; i++) {
+		//				System.out.println(num + "×" + i + "は" + num * i+"です");
+		//			}
+		//		}
+
+		//3-2
+		//		for (int i = 0; i <= 100; i++) {
+		//			if ((i % 3 == 0 || i % 5 == 0) && !(i % 15 == 0)) {
+		//				System.out.println(i);
+		//			}
+		//		}
+
+		//3-3
+//		for (int i = 0; i <= 100; i++) {
+//			for (int n = 0; n <= 100; n++) {
+//				int temp = i + n;
+//				if (temp % 3 == 0 && !(temp == 0)) {
+//					System.out.println(i + "タス" + n);
+//				}
+//
+//			}
+//		}
+
+	}
+}
